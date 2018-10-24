7330493f31c92a8fa49c1fb25770559a7dd8d6d0
Maria Hovgaard Rasmussen <Mari55fe@edu.eal.dk>
2018-10-24 09:52:43 +0200

    Tilføjet kode til amulet

--- Ex16-Morgengry/Amulet.cs
+++ Ex16-Morgengry/Amulet.cs
@@ -6,9 +6,52 @@ using System.Threading.Tasks;
 
 namespace Ex16_Morgengry
 {
-    class Amulet
+    public class Amulet
     {
-        
+      
+
+        private string itemid; // private data
+        private string design; // private data
+
+        public enum Level
+        {
+            Low,
+            Medium,
+            High
+        };
+
+        Level quality;
+
+        public Amulet (string itemid, string design, Level quality)
+        {
+            Itemid = itemid; // parameter ned i mellem mand
+            Design = design; // parameter ned i mellem mand
+        }
+
+        public string Itemid // Mellem mand
+        {
+            get
+            {
+                return itemid;
+            }
+
+            set
+            {
+                itemid = value; // lægger value ned i vores private data
+            }
+        }
+        public string Design // Mellem mand
+        {
+            get
+            {
+                return design;
+            }
+
+            set
+            {
+                design = value;  // lægger value ned i vores private data
+            }
+        }
     }
 
     class Book
