17d45cebdd8c5f1d2e83b8ee16b8a157bad8434d
fais0078 <fais0078@edu.eal.dk>
2018-10-29 23:16:20 +0100

    har lavet "Book og amulet klasse"

---
+++ Ex16-Morgengry/AmuletRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex16_Morgengry
+{
+    public class AmuletRepository
+    {
+        private List<Amulet> amulets; 
+
+        public AmuletRepository()
+        {
+            amulets = new List<Amulet>();
+        }
+        public void Addamulet(Amulet amulet)
+        {
+            amulets.Add(amulet);
+        }
+
+        public Amulet GetAmulet(string itemid)
+        {
+            Amulet resultat = null;
+
+            for (int i = 0; i < amulets.Count; i++)
+            {
+                if (amulets[i].ItemId.Equals(itemid))
+                {
+                    resultat = amulets[i];
+                }
+                return resultat;
+            }
+            
+        }
+    }
+}
---
+++ Ex16-Morgengry/BookRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex16_Morgengry
+{
+   public class BookRepository
+    {
+        private List<Book> books;
+
+        public BookRepository()
+        {
+            books = new List<Book>(); 
+        }
+
+
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+
+        public Book GetBook(string itemid)
+        {
+            Book resultat = null;
+
+            for (int i = 0; i < books.Count ; i++)
+            {
+                if (books[i].ItemId.Equals(itemid))
+                {
+                    resultat = books[i];
+                }
+            }
+            return resultat;
+        }
+
+
+
+        public double GetTotalValue()
+        {
+            double total = 0;
+
+            for (int i = 0; i < books.Count ; i++)
+            {
+                total += Utility.GetValueOfBook(books[i]); //+= betyder at man lægger til
+            }
+            return total;
+        }
+
+    }
+}
