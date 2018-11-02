010abf7a9142107a4fb4c5d36cdf1186ce4ed2f5
Maria Hovgaard Rasmussen <Mari55fe@edu.eal.dk>
2018-11-02 12:10:33 +0100

    ValuableRepository

---
+++ Ex16-Morgengry/IValuable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex16_Morgengry
+{
+    public interface IValuable
+    {
+        double GetValue();
+
+    }
+}
---
+++ Ex16-Morgengry/ValuableRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex16_Morgengry
+{
+    public class ValuableRepository
+    {
+        private List<IValuable> valuables;
+
+        public ValuableRepository()
+        {
+            valuables = new List<IValuable>();
+        }
+        public void AddIValuable(IValuable valuable)
+        {
+            valuables.Add(valuable);
+        }
+
+        public IValuable GetValuable(string id)
+        {
+            IValuable result = null;
+            valuables.ForEach(x =>
+           {
+               if (x is Merchandise merchandise)
+               {
+                   result = x;
+               }
+               else if (x is Course course)
+               {
+                   result = x;
+               }
+           });
+            return result;
+        }
+
+
+    }
+}
